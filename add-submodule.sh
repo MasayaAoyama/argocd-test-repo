23de2e15854c3c4d6ed42eaca819155303133422
Masaya Aoyama (@amsy810) <amsy810@gmail.com>
2024-03-27 10:28:10 +0900

    fix

---
+++ add-submodule.sh
@@ -0,0 +1,3 @@
+git submodule add https://github.com/kubernetes/kubernetes.git  manysecret/kubernetes
+git submodule add https://github.com/argoproj/argo-cd.git  manyconfigmap/argo-cd
+
---
+++ rm-submodule.sh
@@ -0,0 +1,4 @@
+git submodule deinit manyconfigmap/argo-cd
+git submodule deinit manysecret/kubernetes
+rm -rf manyconfigmap/argo-cd
+rm -rf manysecret/kubernetes
